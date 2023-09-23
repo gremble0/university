32573494254e4ec5d00c998ef9e4ccca02d3e282
gremble0 <hermanstornes@gmail.com>
2023-09-23 07:20:35 +0200

    Completed task 1a-1b

--- IN2040/oblig2a.scm
+++ IN2040/oblig2a.scm
@@ -1,10 +1,25 @@
 ;; Oppgave 1
 ;;; a:
 (define (p-cons x y)
-  (lambda (l) (l x y)))
+  (lambda (proc) (proc x y)))
 
-(define (p-car l)
-  (l (lambda (x y) x)))
+(define (p-car proc)
+  (proc (lambda (x y) x)))
 
-(define (p-cdr l)
-  (l (lambda (x y) y)))
+(define (p-cdr proc)
+  (proc (lambda (x y) y)))
+
+;;; b:
+(define foo 42)
+
+((lambda (foo x)
+   (if (= x foo)
+       'same
+       'different))
+ 5 foo) ;; => different
+
+((lambda (bar baz)
+   ((lambda (bar foo)
+      (list foo bar))
+    (list bar baz) baz))
+ foo 'towel) ;; => (towel (42 towel))
