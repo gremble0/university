a29f56e9dd2666019e79856e1f7459e6f3060789
gremble0 <hermanstornes@gmail.com>
2023-09-24 05:23:27 +0200

    Completed up to task 2c

---
+++ IN2040/huffman.scm
@@ -0,0 +1,101 @@
+;;;;
+;;;; Prekode til innlevering 2a i IN2040 (H23): Prosedyrer for å jobbe med
+;;;; Huffman-trær, fra SICP, Seksjon 2.3.4.
+;;;;
+
+;;; Merk at koden under gjør bruk av diverse innebygde kortformer for
+;;; kjeder av car og cdr. F.eks er (cadr x) det samme som (car (cdr x)), 
+;;; og (caadr x) tilsvarer (car (car (cdr x))), osv. 
+
+
+
+;;;
+;;; Abstraksjonsbarriere for trær:
+;;;
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+
+;;;
+;;; Dekoding:
+;;;
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (if (= bit 0) 
+      (left-branch branch)
+      (right-branch branch)))
+
+
+;;;
+;;; Sortering av node-lister:
+;;;
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)
+                               (cadr pair))
+                    (make-leaf-set (cdr pairs))))))
+
+
+;;; Eksempel på kodebok:
+(define sample-tree
+  (make-code-tree
+    (make-code-tree
+      (make-leaf 'fight 6)
+      (make-leaf 'ninjas 5))
+    (make-code-tree
+      (make-leaf 'samurais 4)
+      (make-code-tree
+        (make-leaf 'night 2)
+        (make-leaf 'by 1)))))
+
+;;; Eksempelkode: 
+(define sample-code '(1 0 0 0 0 1 1 1 1 1 1 0))
--- IN2040/oblig2a.scm
+++ IN2040/oblig2a.scm
@@ -1,3 +1,6 @@
+;; Importer prekode (må evalueres før kode fra oppgave 2)
+(load "huffman.scm")
+
 ;; Oppgave 1
 ;;; a:
 (define (p-cons x y)
@@ -23,3 +26,43 @@
       (list foo bar))
     (list bar baz) baz))
  foo 'towel) ;; => (towel (42 towel))
+
+;;; c:
+;;;; Prosedyrekallet nedenfor fører til en error siden prosedyren forventer at det midterste
+;;;; elementet i listen skal være en prosedyre og ikke et uevaluert symbol som '/
+(define (infix-eval l)
+  ((cadr l) (car l) (caddr l)))
+
+(define bah '(84 / 2))
+(infix-eval bah) ;; => error
+
+;; Oppgave 2
+;;; a:
+(define (decode bits tree)
+  (define (decode-impl bits current-branch out)
+    (if (null? bits)
+        (reverse out)
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (decode-impl (cdr bits) tree (cons (symbol-leaf next-branch) out))
+              (decode-impl (cdr bits) next-branch out)))))
+  (decode-impl bits tree '()))
+
+;;; b:
+(decode sample-code sample-tree) ;; => (samurais fight ninjas by night)
+
+;;; c:
+(define (encode message tree)
+  (define (encode-impl message current-branch out)
+    (if (null? message)
+        (reverse out)
+        (cond ((leaf? current-branch)
+               (encode-impl (cdr message) tree out))
+              ((memq (car message) (symbols (right-branch current-branch)))
+               (encode-impl message (right-branch current-branch) (cons 1 out)))
+              (else
+               (encode-impl message (left-branch current-branch) (cons 0 out))))))
+  (encode-impl message tree '()))
+
+(decode (encode '(ninjas fight ninjas) sample-tree) sample-tree) ;; => (ninjas fight ninjas)
