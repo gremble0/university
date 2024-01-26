3ec5adb0035b93baa469f7e5959efe5a4a7914fd
gremble0 <hermanstornes@gmail.com>
2024-01-26 21:01:04 +0100

    Init gradle project for in3030

---
+++ IN3030/app/src/main/java/in3030/SortNumbers.java
@@ -0,0 +1,11 @@
+package in3030;
+
+public class SortNumbers {
+    public static void sortSequential(int[] numbers, int k) {
+        return;
+    }
+
+    public static void sortParallel(int[] numbers, int k) {
+        return;
+    }
+}
---
+++ IN3030/app/src/test/java/in3030/SortNumbersTest.java
@@ -0,0 +1,13 @@
+package in3030;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Random;
+
+class SortNumbersTest {
+    @Test
+    void sortNumbersSequential(int k) {
+    }
+}
