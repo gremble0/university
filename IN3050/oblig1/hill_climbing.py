80f9f1d49716c113f6466914e25b3494c56735ef
gremble0 <hermanstornes@gmail.com>
2024-02-14 16:41:02 +0100

    finish hill_climbing (need to make better_neighbor)

--- IN3050/oblig1/hill_climbing.py
+++ IN3050/oblig1/hill_climbing.py
@@ -8,9 +8,14 @@ def hill_climbing(city_coordinates: dict[str, list[float]]) -> list[str]:
     all_permutations = list(permutations(city_coordinates.keys()))
 
     best_solution = random.choice(all_permutations)
-    best_solution_fitness = fitness(best_solution)
+    prev_best_solution: tuple[str, ...] | None = None
 
-    while ...:
-        pass
+    while best_solution != prev_best_solution:
+        prev_best_solution = best_solution
+        best_solution = better_neighbor(best_solution)
 
     return list(best_solution)
+
+
+def better_neighbor(solution: tuple[str, ...]) -> tuple[str, ...]:
+    pass
