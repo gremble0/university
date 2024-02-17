24d2e014423bd6d485cbaf29265ebd445e909f9d
gremble0 <hermanstornes@gmail.com>
2024-02-17 11:17:03 +0100

    implement mutate and crossover functions

--- IN3050/oblig1/genetic_algorithm.py
+++ IN3050/oblig1/genetic_algorithm.py
@@ -7,17 +7,17 @@ def genetic_algorithm(
 ) -> list[str]:
     population = random.sample(list(city_coordinates.keys()), population_size)
 
-    while True:
-
-
     return []
 
 def mutate(solution: list[str]) -> list[str]:
-    # swap 2 random cities
-    return []
+    city1_i, city2_i = random.sample(range(len(solution)), 2)
+
+    new_solution = solution[:] # copy to not mutate input
+    new_solution[city1_i], new_solution[city2_i] = solution[city2_i], solution[city1_i]
+
+    return new_solution
 
 def crossover(solution1: list[str], solution2: list[str]) -> list[str]:
-    return []
+    split_index = random.choice(range(len(solution1)))
 
-def n_crossover(*solutions: list[str]) -> list[str]:
-    return []
+    return [solution1[i] if i < split_index else solution2[i] for i in range(len(solution1))]
