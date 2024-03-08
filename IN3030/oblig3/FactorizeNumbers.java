f2776b8e3fcbe78cd7c23b12a07e25eff73326f2
gremble0 <hermanstornes@gmail.com>
2024-03-08 14:56:53 +0100

    I guess we need things in the same class

--- IN3030/oblig3/FactorizeNumbers.java
+++ IN3030/oblig3/FactorizeNumbers.java
@@ -1,106 +1,119 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class FactorizeNumbers {
-  private final long n;
-  private final int k, intervalSize, cores;
-  private final Oblig3Precode precode;
-  private final List<List<Long>> factors = new ArrayList<>();
-
-  private class FactorizeNumber implements Runnable {
-    private long num;
-    private List<Long> numFactors;
-
-    FactorizeNumber(long num) {
-      this.num = num;
-      this.numFactors = new ArrayList<Long>();
-    }
-
-    public void run() {
-      numFactors.add(Long.valueOf(2));
-
-      // TODO: is this thread safe?
-      factors.add(numFactors);
-    }
-  }
-
-  private class FactorizeNumbersInInterval implements Runnable {
-    private final Thread[] threads;
-    private final int start, end;
-
-    /**
-     * @param precode to log all added factors
-     * @param start   the lower number in the range max..max-n
-     * @param end     the upper number in the range max..max-n, is higher than start
-     */
-    FactorizeNumbersInInterval(int start, int end) {
-      this.start = start;
-      this.end = end;
-      this.threads = new Thread[end - start];
-    }
-
-    public void run() {
-      for (int i = 0; i < threads.length; i++) {
-        threads[i] = new Thread(new FactorizeNumber(n - start - i));
-        threads[i].start();
-      }
-
-      try {
-        for (int i = 0; i < threads.length; i++) {
-          threads[i].join();
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-  }
-
-  /**
-   * @param n       biggest number to factorize
-   * @param k       how many numbers below n to factorize
-   * @param cores   how many cores to allocate for the methods of this class
-   * @param precode used to track factors
-   */
-  FactorizeNumbers(long n, int k, int cores, Oblig3Precode precode) {
-    this.n = n;
-    this.k = k;
-    this.cores = cores;
-    this.intervalSize = k / cores;
-    this.precode = precode;
-  }
-
-  public List<List<Long>> factorize() {
-    Thread[] threads = new Thread[cores];
-
-    int start = 0;
-    for (int i = 0; i < cores - 1; i++) {
-      threads[i] = new Thread(new FactorizeNumbersInInterval(start, start + intervalSize));
-      threads[i].start();
-      start += intervalSize;
-    }
-    // Last thread takes the rest of the numbers
-    threads[cores - 1] = new Thread(new FactorizeNumbersInInterval(start, k));
-    threads[cores - 1].start();
-
-    try {
-      for (int i = 0; i < cores; i++) {
-        threads[i].join();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-    return factors;
-  }
-
-  public void writeFactors() {
-    for (int i = 0; i < factors.size(); i++) {
-      List<Long> factorsForI = factors.get(i);
-      for (int j = 0; j < factorsForI.size(); j++) {
-        precode.addFactor(n - i, factorsForI.get(j));
-      }
-    }
-
-    precode.writeFactors();
-  }
-}
+// import java.util.ArrayList;
+// import java.util.List;
+//
+// class FactorizeNumbers {
+// private final long n;
+// private final int k, intervalSize, cores;
+// private final Oblig3Precode precode;
+// private final List<List<Long>> factors = new ArrayList<>();
+//
+// private class FactorizeNumber implements Runnable {
+// private long num;
+// private List<Long> numFactors;
+//
+// FactorizeNumber(long num) {
+// this.num = num;
+// this.numFactors = new ArrayList<Long>();
+// }
+//
+// public void run() {
+// long i = 2;
+// while (i < num) {
+// System.out.println(i + " " + num);
+// if (num % i == 0) {
+// numFactors.add(Long.valueOf(i));
+// num /= i;
+// i = 2;
+// } else {
+// ++i;
+// // TODO: nextPrime
+// // i = nex
+// }
+// }
+//
+// // TODO: is this thread safe?
+// factors.add(numFactors);
+// }
+// }
+//
+// private class FactorizeNumbersInInterval implements Runnable {
+// private final Thread[] threads;
+// private final int start, end;
+//
+// /**
+// * @param precode to log all added factors
+// * @param start the lower number in the range max..max-n
+// * @param end the upper number in the range max..max-n, is higher than start
+// */
+// FactorizeNumbersInInterval(int start, int end) {
+// this.start = start;
+// this.end = end;
+// this.threads = new Thread[end - start];
+// }
+//
+// public void run() {
+// for (int i = 0; i < threads.length; i++) {
+// threads[i] = new Thread(new FactorizeNumber(n - start - i));
+// threads[i].start();
+// }
+//
+// try {
+// for (int i = 0; i < threads.length; i++) {
+// threads[i].join();
+// }
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
+// }
+// }
+//
+// /**
+// * @param n biggest number to factorize
+// * @param k how many numbers below n to factorize
+// * @param cores how many cores to allocate for the methods of this class
+// * @param precode used to track factors
+// */
+// FactorizeNumbers(long n, int k, int cores, Oblig3Precode precode) {
+// this.n = n;
+// this.k = k;
+// this.cores = cores;
+// this.intervalSize = k / cores;
+// this.precode = precode;
+// }
+//
+// public List<List<Long>> factorize() {
+// Thread[] threads = new Thread[cores];
+//
+// int start = 0;
+// for (int i = 0; i < cores - 1; i++) {
+// threads[i] = new Thread(new FactorizeNumbersInInterval(start, start +
+// intervalSize));
+// threads[i].start();
+// start += intervalSize;
+// }
+// // Last thread takes the rest of the numbers
+// threads[cores - 1] = new Thread(new FactorizeNumbersInInterval(start, k));
+// threads[cores - 1].start();
+//
+// try {
+// for (int i = 0; i < cores; i++) {
+// threads[i].join();
+// }
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
+//
+// return factors;
+// }
+//
+// public void writeFactors() {
+// for (int i = 0; i < factors.size(); i++) {
+// List<Long> factorsForI = factors.get(i);
+// for (int j = 0; j < factorsForI.size(); j++) {
+// precode.addFactor(n - i, factorsForI.get(j));
+// }
+// }
+//
+// precode.writeFactors();
+// }
+// }
--- IN3030/oblig3/SieveOfEratosthenesPara.java
+++ IN3030/oblig3/SieveOfEratosthenesPara.java
@@ -1,8 +1,82 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class SieveOfEratosthenesPara {
-  final int cores;
-  int n, root;
-  int[] numsOfPrimes;
-  byte[] oddNumbers;
+  private final int cores;
+  private int n, root;
+  private int[] numsOfPrimes;
+  private byte[] oddNumbers;
+  private final Oblig3Precode precode;
+  private final List<List<Long>> factors = new ArrayList<>();
+
+  private class FactorizeNumber implements Runnable {
+    private final long num;
+    private List<Long> numFactors;
+
+    FactorizeNumber(long num) {
+      this.num = num;
+      this.numFactors = new ArrayList<Long>();
+    }
+
+    public void run() {
+      int i = 2;
+      long numIter = num;
+
+      while (i < numIter) {
+        if (num % i == 0) {
+          numFactors.add(Long.valueOf(i));
+          numIter /= i;
+          i = 2;
+          continue;
+        }
+
+        int nextP = nextPrime(i);
+        if (nextP == -1) {
+          break;
+        } else {
+          i = nextP;
+        }
+      }
+
+      if (numFactors.size() == 0) {
+        numFactors.add(num);
+      }
+
+      // TODO: is this thread safe?
+      factors.add(numFactors);
+    }
+  }
+
+  private class FactorizeNumbersInInterval implements Runnable {
+    private final Thread[] threads;
+    private final int start;
+
+    /**
+     * @param precode to log all added factors
+     * @param start   the lower number in the range max..max-n
+     * @param end     the upper number in the range max..max-n, is higher than start
+     */
+    FactorizeNumbersInInterval(int start, int end) {
+      this.start = start;
+      this.threads = new Thread[end - start];
+    }
+
+    public void run() {
+      for (int i = 0; i < threads.length; i++) {
+        threads[i] = new Thread(new FactorizeNumber(n - start - i));
+        threads[i].start();
+      }
+
+      try {
+        for (int i = 0; i < threads.length; i++) {
+          threads[i].join();
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+
+  }
 
   /**
    * Constructor that initializes the global variables
@@ -15,6 +89,7 @@ class SieveOfEratosthenesPara {
     this.root = (int) Math.sqrt(n);
     this.oddNumbers = new byte[(n / 16) + 1];
     this.numsOfPrimes = new int[cores];
+    this.precode = new Oblig3Precode(n);
   }
 
   private class SieveInInterval implements Runnable {
@@ -46,25 +121,61 @@ class SieveOfEratosthenesPara {
       }
     }
 
-    /**
-     * Finds the next prime after some odd number.
-     *
-     * @param prev the number to start looking for prime numbers after. This must be
-     *             an odd number
-     * @return the first prime after prev
-     */
-    private int nextPrime(int prev) {
-      for (int i = prev + 2; i <= end; i += 2)
-        if (isPrime(i))
-          return i;
-
-      return -1;
-    }
-
     private void traverse(int prime) {
       for (int i = prime * prime; i <= n; i += prime * 2)
         mark(i);
     }
+  }
+
+  public List<List<Long>> factorize(int k) {
+    final int intervalSize = k / cores;
+    Thread[] threads = new Thread[cores];
+
+    int start = 0;
+    for (int i = 0; i < cores - 1; i++) {
+      threads[i] = new Thread(new FactorizeNumbersInInterval(start, start + intervalSize));
+      threads[i].start();
+      start += intervalSize;
+    }
+    // Last thread takes the rest of the numbers
+    threads[cores - 1] = new Thread(new FactorizeNumbersInInterval(start, k));
+    threads[cores - 1].start();
+
+    try {
+      for (int i = 0; i < cores; i++) {
+        threads[i].join();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return factors;
+  }
+
+  public void writeFactors() {
+    for (int i = 0; i < factors.size(); i++) {
+      List<Long> factorsForI = factors.get(i);
+      for (int j = 0; j < factorsForI.size(); j++) {
+        precode.addFactor(n - i, factorsForI.get(j));
+      }
+    }
+
+    precode.writeFactors();
+  }
+
+  /**
+   * Finds the next prime after some odd number.
+   *
+   * @param prev the number to start looking for prime numbers after. This must be
+   *             an odd number
+   * @return the first prime after prev
+   */
+  private int nextPrime(int prev) {
+    for (int i = prev + 2; i <= root; i += 2)
+      if (isPrime(i))
+        return i;
+
+    return -1;
   }
 
   public int[] getPrimes() {
@@ -192,8 +303,7 @@ class SieveOfEratosthenesPara {
     soe.getPrimes();
     System.out.println(System.nanoTime() - before);
 
-    FactorizeNumbers fn = new FactorizeNumbers((long) n * (long) n, 100, threads, new Oblig3Precode(n));
-    fn.factorize();
-    fn.writeFactors();
+    soe.factorize(100);
+    soe.writeFactors();
   }
 }
