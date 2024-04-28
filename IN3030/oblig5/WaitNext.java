da9ffdb877160b2347cf8658223346f44170c270
gremble0 <hermanstornes@gmail.com>
2024-04-28 05:57:11 +0200

    Start oblig5 in3030

---
+++ IN3030/oblig5/WaitNext.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.Semaphore;
+
+class WaitNext {
+    static Semaphore okToKick = new Semaphore(1, true);
+    static Semaphore holdingArea = new Semaphore(1, true);
+    static boolean first = true;
+    static int i = 0;
+    static int cores = Runtime.getRuntime().availableProcessors();
+
+    static class Worker implements Runnable {
+        public void run() {
+            waitNext();
+        }
+    }
+
+    public static void waitNext() {
+        ++i;
+        System.out.println(i);
+
+        try {
+            okToKick.acquire();
+
+            if (first)
+                first = false;
+            else
+                holdingArea.release();
+
+            okToKick.release();
+            holdingArea.acquire();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread[] ts = new Thread[cores];
+        for (int i = 0; i < cores; i++)
+            (ts[i] = new Thread(new Worker())).run();
+
+        for (Thread thread : ts) {
+            try {
+                thread.join();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+}
